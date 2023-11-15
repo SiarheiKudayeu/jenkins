b8e5c1029ad18aa48e826f525daa9840407263ce
Siarhei <greysiarhei@gmail.com>
2023-11-15 17:51:28 +0300

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("This is you first java job in jenkins.");
+        System.out.println("This is you first test.");
+    }
+}
